42253fdb2e20f35bf68598c618ecf555ebbce605
KB <kyungbong135@gmail.com>
2021-12-08 14:05:08 -0800

    12 write.c change

--- write.c
+++ write.c
@@ -2,7 +2,7 @@
 #include "write.h"
 #include "canvas.h"
 
-void write_line(line* input_line) {
+void write_line(canvas* drawing_canvas, line* input_line, int input) {
   int y_diff = input_line->p1.y - input_line->p2.y;
   int x_diff = input_line->p1.x - input_line->p2.x;
 
@@ -21,15 +21,48 @@ void write_line(line* input_line) {
     input_line->type = vert;
   }
 
-  
+  switch (input_line->type) {
+    case horiz:
+      //Code for horizontal
+      format_x_coords(input_line);
+      for (int i = input_line->p1.x; i < input_line->p2.x; ++i) {
+        (drawing_canvas->array)[input][i] = '-';
+      }
+      break;
+    case vert:
+      format_y_coords(input_line);
+      for (int i = input_line->p1.y; i < input_line->p2.y; ++i) {
+        (drawing_canvas->array)[i][input] = '|';
+      }
+      break;
+    case pos_diag:
+      format_x_coords(input_line);
+      format_y_coords(input_line);
+      int coord_diff = input_line->p1.x - input_line->p1.y;
+      for (int i = input_line->p1.x; i < input_line->p2.x; ++i) {
+        (drawing_canvas->array)[input_line->p1.y + i][input_line->p1.x + coord_diff + i] = '/';
+      }
+      break;
+    case neg_diag:
+      
+      break;
+    default:
+      break;
+  }
+}
 
-  if (input_line->type == horiz) {
-    //Code for horizontal
-  } else if (input_line->type == vert) {
-    //Code for vertical
-  } else if (input_line->type == pos_diag) {
-    //Code for positive diagonal
-  } else if (input_line->type == neg_diag) {
-    //Code for negative diagonal
+void format_x_coords(line* input_line) {
+  if (input_line->p1.x > input_line->p2.x) {
+    int temp = input_line->p1.x;
+    input_line->p1.x = input_line->p2.x;
+    input_line->p2.x = temp;
+  }
+}
+
+void format_y_coords(line* input_line) {
+  if (input_line->p1.y > input_line->p2.y) {
+    int temp = input_line->p1.y;
+    input_line->p1.y = input_line->p2.y;
+    input_line->p2.y = temp;
   }
 }
