d88ebe34c654c33324a81f5630cf0e1c1ad594c4
John Carraher <jkcarraher@gmail.com>
2021-12-09 04:38:24 -0800

    15.2.2 HUH p2

--- load.c
+++ load.c
@@ -9,13 +9,24 @@ canvas* load_canvas(canvas* input_canvas, char* file_name) {
   }
 
   input_canvas = (canvas*)calloc(1, sizeof(canvas));
-  fscanf(input_file, "%d %d", &input_canvas->numRows, &input_canvas->numCols);
   input_canvas->blankChar = '*';
+  get_dimensions(input_file, &input_canvas->numRows, &input_canvas->numCols);
   makeCanvas(input_canvas);
   for (int i = 0; i < input_canvas->numRows; ++i) {
     for (int j = 0; j < input_canvas->numCols; ++j) {
       fscanf(input_file, " %c", &((input_canvas->array)[i][j]));
     }
   }
+  close_txt(input_file);
   return input_canvas;
+}
+
+void get_dimensions(FILE* my_txt, int* length, int* width){
+  fscanf(my_txt,"%d %d", length, width);
+  return;
+}
+
+
+void close_txt(FILE* my_txt){
+  fclose(my_txt);
 }
--- load.h
+++ load.h
@@ -2,4 +2,6 @@
 #define LOAD_H
   #include "canvas.h"
   canvas* load_canvas(canvas* input_canvas, char* file_name);
+  void get_dimensions(FILE* my_txt, int* length, int* width){
+  void read_txt_into_array(FILE* my_txt, double*** matrix, int dimension);
 #endif
