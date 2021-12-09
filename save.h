78fa89147ec3e2c73d0cad343c546a4124a92b8f
KB <kyungbong135@gmail.com>
2021-12-09 01:53:29 -0800

    14 just .o files

--- save.h
+++ save.h
@@ -1,10 +1,8 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include "save.h"
 #include "canvas.h"
 
 #ifndef SAVE_H
 #define SAVE_H
-  #include "save.h"
   void save_canvas(canvas* drawingCanvas, char* file_name);
 #endif
