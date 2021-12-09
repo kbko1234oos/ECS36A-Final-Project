1ced2904bd35fca0bd60d04b9e197b83a11645f5
KB <kyungbong135@gmail.com>
2021-12-08 22:17:11 -0800

    12.6.2 resize.h change

--- resize.h
+++ resize.h
@@ -1,6 +1,6 @@
 #ifndef RESIZE_H
 #define RESIZE_H
     #include "canvas.h"
-    void resize_canvas(canvas* old_canvas, int input_row, int input_col);
+    canvas* resize_canvas(canvas* old_canvas, int input_row, int input_col);
     void copy_canvas(canvas* old_canvas, canvas* new_canvas, int input_row, int input_col);
 #endif
